dc3b44e06a428f4af6cc74e3e34abecf9b82ded7
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-11-10 16:00:03 +0000

    order画面view作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,18 +1,22 @@
 class Public::OrdersController < ApplicationController
   
   def new
+    @order = current_customer
   end
   
   def confirm
+    @order = current_customer
   end
   
   def thanks
   end
  
   def create
+    @order = current_customer
   end
 
   def index
+    @order = current_customer
   end
   
   def show
