53472603b51096f15f0b24eede471bb6a1f133c3
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-11-11 16:27:55 +0000

    カート機能作成途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,15 +5,27 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    binding.pry
+    
+    cart_item = cart_item.new(cart_item_params)
+    cart_item.save
+    redirect_to cart_items_path
   end
 
   def update
+    
   end
 
   def destroy
   end
 
   def destroy_all
+  end
+  
+  private
+  
+  def cart_item_params
+    prams.require(:cart_item).permit(:item_id, :amount)
   end
 
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  bedore_action :customer_state, only: [:create]
   
   def adter_sign_out_path_for(resource)
     new_customer_session_path
@@ -32,4 +33,14 @@ class Public::SessionsController < Devise::SessionsController
    def configure_sign_in_params
      devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
    end
+   
+   def customer_state
+     @customer = Customer.find_by(email: params[:customer][:email])
+     
+    return if !@customer
+    
+    if @customer.valid_password?(params[:customer][:password])
+    end  
+   end
+   
 end
