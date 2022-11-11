1f170adea313f3cbe787f859f7c1688a054564a9
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-11-11 15:00:00 +0000

    退会処理boolean導入

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,13 +3,13 @@
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  def after_sign_out_path_for(resource)
-    new_admin_session_path
-  end
+  #def after_sign_out_path_for(resource)
+   # new_admin_session_path
+  #end
 
-  def after_sign_in_path_for(resource)
-    admin_path
-  end
+  #def after_sign_in_path_for(resource)
+  #  admin_path
+  #end
   # GET /resource/sign_in
   # def new
   #   super
@@ -25,10 +25,10 @@ class Admin::SessionsController < Devise::SessionsController
    #  super
    #end
 
-   protected
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_in_params
-     devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
-   end
+   #def configure_sign_in_params
+   #  devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+   #end
 end
