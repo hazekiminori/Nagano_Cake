c4971862d00714313fcf5d7a14e7ce65ca7c27f3
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-11-13 17:21:44 +0000

    カート追加機能実装途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,11 +6,15 @@ class Public::CartItemsController < ApplicationController
 
   def create
     binding.pry
-   
-    @cart_item = Cart_item.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save
-    redirect_to cart_items_path
+    @cart_item = Cart_items.new(cart_item_params)
+    if Item.find_by(item_id: [:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+    else
+      @cart_item = Cart_item.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def update
