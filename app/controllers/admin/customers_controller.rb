370dbed44973adcb475edd8d07c3a0e0d1facbf6
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-11-15 18:15:12 +0000

    管理者の顧客情報編集画面まで完成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,5 @@
 class Admin::CustomersController < ApplicationController
 
-  before_action :set_customer, only:[:edit, :update]
-
   def index
     @customers = Customer.page(params[:page])
   end
@@ -23,10 +21,6 @@ class Admin::CustomersController < ApplicationController
   end
 
   private
-
-  def set_customer
-    @customer = Customer.find(current_user.id)
-  end
 
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
