cfd58b0a979522c4fd6790f9e6d38f879ebe7f58
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-11-21 08:18:42 +0000

    注文確認画面実装途中

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,9 @@
+class Admin::OrdersController < ApplicationController
+  
+  def show
+    @customer = Customer.find(params[:id])
+    @order = Order.find(params[:id])
+    
+  end
+  
+end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,18 +4,6 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
 
-  def confirm
-    @order = Order.new(order_params)
-    @address = Address.find(params[:order][:address_id])
-    @order.postal_code = @address.postal_code
-    @order.address = @address.address
-    @order.name = @address.name
-    binding.pry
-  end
-
-  def thanks
-  end
-
   def create
     binding.pry
     @order = Order.new(order_params)
@@ -23,8 +11,31 @@ class Public::OrdersController < ApplicationController
     redirect_to confirm_path
   end
 
+  def confirm
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    
+    @order = Order.new(order_params)
+    if params[:order][:address_id] == "0"
+      @order.postal_code= current_customer.postal_code
+      @order.address= current_customer.address
+      @order.name= current_customer.full_name
+    elsif params[:order][:address_id] == "1"
+      @order.postal_code= Address.postal_code
+      @order.address= Address.address
+      @order.name= Address.name
+    else params[:order][:address_id] == "2"
+      @order.postal_code
+      @order.address
+      @order.name
+    #binding.pry
+    end
+  end
+
+  def thanks
+  end
+
   def index
-    @order = current_customer
+    @order = current_customer.order
   end
 
   def show
@@ -37,6 +48,5 @@ class Public::OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
-
 
 end
