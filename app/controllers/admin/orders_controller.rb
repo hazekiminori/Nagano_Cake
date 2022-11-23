b71dd6254b62683f0d13f02bf44d7736aa96fbce
EC2 Default User <ec2-user@ip-172-31-30-106.ap-northeast-1.compute.internal>
2022-11-23 17:32:03 +0000

    管理者画面、注文履歴一覧の注文個数が表示できない

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,7 +3,9 @@ class Admin::OrdersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
     @order = Order.find(params[:id])
-    @cart_items = Cart_item.where(customer_id: current_customer)
+    @total=0
+    @order.order_details.each do |order_detail|
+    @total += (order_detail.price*1.1).floor*order_detail.amount
+    end
   end
-  
 end
